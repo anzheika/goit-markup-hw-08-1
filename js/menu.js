245497011149488f614084d7e91120fbe19bae8e
anzheika <thedipterus@gmail.com>
2021-11-03 17:46:35 +0200

    f

--- js/menu.js
+++ js/menu.js
@@ -1,14 +1,21 @@
 (() => {
   const refs = {
-    openMenulBtn: document.querySelector('[menu-open-btn]'),
-    closeMenuBtn: document.querySelector('[mob-menu-close-btn]'),
-    menu: document.querySelector('[mob-menu-backdrop]'),
+    openMenuBtn: document.querySelector('.menu-open-btn'),
+    closeMenuBtn: document.querySelector('.menu-close-btn'),
+    menu: document.querySelector('.backdrop'),
+    menuItem: document.querySelector('.mob-menu'),
   };
 
   refs.openMenuBtn.addEventListener('click', toggleMenu);
   refs.closeMenuBtn.addEventListener('click', toggleMenu);
+  refs.menu.addEventListener('click', removeMenu);
 
   function toggleMenu() {
     refs.menu.classList.toggle('is-hidden');
+  }
+  function removeMenu(e) {
+    if (e.target === refs.menu) {
+      refs.menu.classList.add('is-hidden');
+    }
   }
 })();
